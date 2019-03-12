7d740b8fbb17214a6542749834c44988ebed2d14
RealFilllykung <filllykunggm@gmail.com>
2019-03-12 16:51:16 +0700

    Initialize the basic condition -	fork() the child -	Create if parent else child *	Not include the step of sending and receiving message yet

--- schat.c
+++ schat.c
@@ -16,12 +16,36 @@ struct shm_st
     char data_1[BUFSIZ];
 };
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
     int shmID, child;
     void *sh_mem = NULL;
     struct shm_st *sh_area;
     char buffer[BUFSIZ] = "";
+    int user = atoi(argv[1]);
     
+    //fork starting!
+    child = fork();
     
+    //if it is parent
+    if(pid){
+        //We use parent as the writer
+        if (user == 1){
+            //If the user login as the user 1
+            
+        }
+        else{
+            
+        }
+    }
+    else{
+        //We use child as the printer
+        if (user == 1){
+            //If the user login as the user 1
+            
+        }
+        else{
+            
+        }
+            
+    }
 }
