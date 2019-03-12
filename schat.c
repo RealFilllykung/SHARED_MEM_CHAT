bf25de37b3ad754a53ae6c1595d32b490b01a010
RealFilllykung <filllykunggm@gmail.com>
2019-03-12 12:08:10 +0700

    Added basic structure of Shared Memory -	Initialize the structure of our shared memory -	Include the essential header

--- schat.c
+++ schat.c
@@ -1,1 +1,27 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/smh.h>
+
+#define MEM_SIZE 4096
+
+struct shm_st
+{
+    int written_0;
+    char data_0[BUFSIZ];
+    int written_1;
+    char data_1[BUFSIZ];
+};
+
+int main(int argc, char *argv[])
+{
+    int shmID, child;
+    void *sh_mem = NULL;
+    struct shm_st *sh_area;
+    char buffer[BUFSIZ] = "";
+    
+    
+}
